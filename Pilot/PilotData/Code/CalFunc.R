c446bd98599e070a72ddd4a1e954c181212cad82
dlcen <dlcen@protonmail.com>
2019-11-09 11:47:59 +0000

    Change the variable names for the hit rates and false alarm rates

--- Pilot/PilotData/Code/CalFunc.R
+++ Pilot/PilotData/Code/CalFunc.R
@@ -1,57 +1,57 @@
-# Accuracy can be calculated as choosing "Seen" or "Familiar" to seen objects
-AccuracyCal <- function(ActualResp, CorrResp){
+# Hit can be calculated as choosing "Seen" or "Familiar" to seen objects
+SFHitCal <- function(ActualResp, CorrResp){
 	if (CorrResp == "Seen" & ActualResp %in% c("Seen", "Familiar")) {
-		Accuracy <- 1
+		Hit <- 1
 	} else if (CorrResp == "New" & ActualResp == "New") {
-		Accuracy <- 1
+		Hit <- 1
 	} else {
-		Accuracy <- 0
+		Hit <- 0
 	}	
 
-	return(Accuracy)
+	return(Hit)
 }
 
-# Accuracy can be calculated as only choosing "Seen" to seen objects
-SeenAccuracyCal <- function(ActualResp, CorrResp){
+# Hit can be calculated as only choosing "Seen" to seen objects
+SHitCal <- function(ActualResp, CorrResp){
   if (CorrResp == "Seen" & ActualResp == "Seen") {
-    Accuracy <- 1
+    Hit <- 1
   } else if (CorrResp == "New" & ActualResp == "New") {
-    Accuracy <- 1
+    Hit <- 1
   } else {
-    Accuracy <- 0
+    Hit <- 0
   } 
   
-  return(Accuracy)
+  return(Hit)
 }
 
-FalseAlarmCal <- function(ActualResp, CorrResp){
+SFFalseHitCal <- function(ActualResp, CorrResp){
   if (CorrResp == 'New' & ActualResp %in% c('Seen', 'Familiar')) {
-    FalseSeen <- 1
+    FalseHit <- 1
   } else {
-    FalseSeen <- 0
+    FalseHit <- 0
   }
 }
 
-FalseSeenHitCal <- function(ActualResp, CorrResp){
+SFalseHitCal <- function(ActualResp, CorrResp){
   if (CorrResp == 'New' & ActualResp == 'Seen') {
-    FalseSeen <- 1
+    FalseHit <- 1
   } else {
-    FalseSeen <- 0
+    FalseHit <- 0
   }
 }
 
-# Context memory
-ContextAccuracyCal <- function(ActualResp, CorrResp) {
+# Source memory
+SourceHitCal <- function(ActualResp, CorrResp) {
   if (ActualResp == CorrResp) {
-    Accuracy = 1
+    Hit = 1
   } else {
-    Accuracy = 0
+    Hit = 0
   }
   
-  return(Accuracy)
+  return(Hit)
 }
 
-ContextFalseHitCal <- function(ActualResp, CorrResp) {
+SourceFalseHitCal <- function(ActualResp, CorrResp) {
   if (CorrResp == "None" & ActualResp != "None") {
       FalseHit = 1}
   else {
