46df4a838e981fdb28dfc362ce8c76ad78e8692c
dlcen <dlcen@protonmail.com>
2019-12-15 15:18:57 +0000

    Added a list of objects (N = 156)

--- BehaviouralParadigm/Preparation/GetObjectList.R
+++ BehaviouralParadigm/Preparation/GetObjectList.R
@@ -1,3 +1,16 @@
+library(data.table)
+
+setwd("C:/Users/c1254825/Desktop/Git/CuriosityVRBehaviouralParadigmEncodingTask/EncodingTask/Assets/Prefabs/Objects")
+
+setwd("C:/Users/c1254825/Desktop/Git/CuriosityVRBehaviouralParadigmEncodingTask/EncodingTask/Assets/Resources/OutsideObjects")
+
+setwd("C:/Users/c1254825/Desktop/Git/CuriosityVRBehaviouralParadigmEncodingTask/EncodingTask/Assets/Resources/OutsideObjects")
+
+present.objects <- list.files(pattern = "\\.prefab$")
+present.object.list <- data.table(Object = sapply(present.objects, function(x) strsplit(x, ".prefab")[[1]], USE.NAMES = FALSE))
+
+write.csv(present.object.list, "C:/Users/c1254825/Desktop/Git/CuriosityVRParadigm/BehaviouralParadigm/Preparation/AllObjectList.csv", row.names = FALSE)
+
 inside.objects <- list.files(pattern = "\\.prefab$")
 inside.object.list <- data.table(Object = sapply(inside.objects, function(x) strsplit(x, ".prefab")[[1]], USE.NAMES = FALSE))
 
@@ -5,3 +18,4 @@ outside.objects <- list.files(pattern = "\\.prefab$")
 outside.object.list <- data.table(Object = sapply(outside.objects, function(x) strsplit(x, ".prefab")[[1]], USE.NAMES = FALSE))
 
 object.list <- rbind(inside.object.list, outside.object.list)
+
