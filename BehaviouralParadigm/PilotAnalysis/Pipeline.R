a255ac7ce02795f7bba1affee0082c747fe416fa
dlcen <dlcen@protonmail.com>
2020-01-17 12:22:16 +0000

    Checked P06 and P07 data

--- BehaviouralParadigm/PilotAnalysis/Pipeline.R
+++ BehaviouralParadigm/PilotAnalysis/Pipeline.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Pipeline of analysing the data
 
 # 1. Preprocessing the data
@@ -18,23 +17,4 @@ source("./PilotAnalysis/Preprocessing.R")
 load("./PilotData/IndividualData.RData")
 
 source("./PilotAnalysis/PlotIndividualData.R")
-=======
-# Pipeline of analysing the data
 
-# 1. Preprocessing the data
-
-is.subset <- FALSE
-
-## For all participants
-if (!is.subset) { participant.list <- list.files(path = "./PilotData/IndividualData/", pattern = "^P")}
-## For a sub-group of participants
-else { participant.list <- c(paste0("P0", c(1:9)), paste0("P", c(11:20)) }
-
-source("./PilotAnalysis/Preprocessing.R")
-
-
-# 2. Plot individual data
-load("./PilotData/IndividualData.RData")
-
-source("./PilotAnalysis/PlotIndividualData.R")
->>>>>>> a9cab5b3b2273e9300d7392ccc5720e7438c7e95
